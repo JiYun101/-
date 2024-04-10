58be99887d9c1dd1607f6cd13ec92678cde85a26
Huang_Jun <1239975159@qq.com>
2024-04-10 20:35:32 +0800

    社团成员管理页面1.0

---
+++ src/main/java/com/msb/club_management/service/MembersService.java
@@ -0,0 +1,18 @@
+package com.msb.club_management.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.msb.club_management.msg.PageData;
+import com.msb.club_management.vo.Members;
+import com.msb.club_management.vo.Users;
+
+public interface MembersService extends BaseService<Members, String>{
+
+    PageData getPageAll(Long pageIndex, Long pageSize, String teamName, String userName);
+
+    PageData getPageMyManId(Long pageIndex, Long pageSize, String teamName, String userName);
+
+
+    boolean isManager(String teamId, String userId);
+
+    Members selectMembes(String id);
+}
