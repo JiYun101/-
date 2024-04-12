a44e8cd65fdbe910f8b995c23d0a67328e926651
wyt <2152959109@qq.com>
2024-04-12 09:15:00 +0800

    修改密码

--- src/main/java/com/msb/club_management/controller/TeamsController.java
+++ src/main/java/com/msb/club_management/controller/TeamsController.java
@@ -1,11 +1,13 @@
 package com.msb.club_management.controller;
 
-
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.msb.club_management.handle.CacheHandle;
 import com.msb.club_management.msg.PageData;
 import com.msb.club_management.msg.R;
 import com.msb.club_management.service.TeamsService;
 import com.msb.club_management.service.UsersService;
+import com.msb.club_management.utils.DateUtils;
+import com.msb.club_management.utils.IDUtils;
 import com.msb.club_management.vo.Teams;
 import com.msb.club_management.vo.Users;
 import org.slf4j.Logger;
@@ -13,9 +15,7 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -84,4 +84,35 @@ public class TeamsController extends BaseController {
         return R.successData(page);
     }
 
+    /**
+     * 根据团长ID获取社团信息
+     */
+    public R getListByManId(String manId) {
+
+        Log.info("根据团长ID获取社团信息，团长ID：{}", manId);
+
+        List<Teams> list = teamsService.getListByManId(manId);
+
+        return R.successData(list);
+    }
+
+    /**
+     * 添加社团
+     */
+    @GetMapping("/add")
+    @ResponseBody
+    public R addTeams(Teams teams) {
+
+        teams.setId(IDUtils.makeIDByCurrent());
+        teams.setCreateTime(DateUtils.getNowDate("yyyy-MM-dd"));
+        teams.setState("1");
+        teams.setUpdateTime(DateUtils.getNowDate("yyyy-MM-dd"));
+        Log.info("添加社团信息，参数：{}", teams);
+
+        int count=teamsService.addTeams(teams);
+        if (count==0){
+            return R.error("社团团长ID无效");
+        }
+        return R.success();
+    }
 }
